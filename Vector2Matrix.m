3b08245ad08f895939a6959159dd39c53039a0f0
leylatulu <53316818+leylatulu@users.noreply.github.com>
2021-04-26 23:39:35 +0300

    Delete Vector2Matrix.m

--- Vector2Matrix.m
+++
@@ -1,8 +0,0 @@
-function [Wg,bh,Wc,bc]=Vector2Matrix(x,S,R)
-Wg = []; bh = []; Wc = []; bc = [];
-for i=1:R
-    Wg = [Wg, x((i-1)*S+1:i*S)];
-end
-bh = x(R*S+1:R*S+S);
-Wc = x(S*(R+1)+1:S*(R+2))';
-bc = x(S*(R+2)+1);
